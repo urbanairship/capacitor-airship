ad6962f103609b10cf5afdd787deaab366bc2a7e
Ryan Lepinski <ryan.lepinski@airship.com>
2024-06-25 11:16:00 -0700

    Cap 6

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "UaCapacitorAirship",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "UaCapacitorAirship",
+            targets: ["UaCapacitorAirship"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
+        .package(url: "https://github.com/urbanairship/airship-mobile-framework-proxy.git", from: "7.0.0")
+    ],
+    targets: [
+        .target(
+            name: "UaCapacitorAirship",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Plugin"
+        )
+    ]
+)
