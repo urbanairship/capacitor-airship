11fbb1cad3bfd5ecac4c70ac85e2da72caff8226
Ryan Lepinski <ryan.lepinski@airship.com>
2025-05-02 09:55:24 -0700

    Darkmode override

--- example/ios/App/App/AirshipPluginExtender.swift
+++ example/ios/App/App/AirshipPluginExtender.swift
@@ -1,25 +1,67 @@
 import Foundation
 import AirshipFrameworkProxy
-import ActivityKit
+import AirshipCore
+import UIKit
+import Combine
 
 @objc(AirshipPluginExtender)
 public class AirshipPluginExtender: NSObject, AirshipPluginExtenderProtocol {
 
-  /// Called on the same run loop when Airship takesOff.
-  @MainActor
-  public static func onAirshipReady() {
-
-    if #available(iOS 16.1, *) {
-      // Throws if setup is called more than once
-      try? LiveActivityManager.shared.setup { configurator in
-
-        // Register each activity type
-          await configurator.register(forType: Activity<LiveActivityExampleAttributes>.self) { attributes in
-          // Track this property as the Airship name for updates
-          attributes.name
+    public static var subscription: AnyCancellable?
+    /// Called on the same run loop when Airship takesOff.
+    @MainActor
+    public static func onAirshipReady() {
+        // Updates Airship windows to use the override
+        AirshipWindowFactory.shared.makeBlock = { scene in
+            let window = UIWindow(windowScene: scene)
+            switch (UserDefaults.standard.theme) {
+            case .dark: window.overrideUserInterfaceStyle = .dark
+            case .light: window.overrideUserInterfaceStyle = .light
+            default: break
+            }
+            return window
         }
-      }
-    }
-  }
 
+        // Optional, update the main window on launch
+        self.updateMainWindowTheme(theme: UserDefaults.standard.theme)
+
+        // Optional, keep the main window in sync
+        subscription = NotificationCenter.default.publisher(for: UserDefaults.didChangeNotification)
+            .map { _ in
+                UserDefaults.standard.theme
+            }
+            .removeDuplicates()
+            .receive(on: RunLoop.main)
+            .sink { theme in
+                Self.updateMainWindowTheme(theme: theme)
+            }
+    }
+
+    private static func updateMainWindowTheme(theme: Theme) {
+        let window = UIApplication.shared.delegate?.window as? UIWindow
+        switch (theme) {
+        case .dark: window?.overrideUserInterfaceStyle = .dark
+        case .light: window?.overrideUserInterfaceStyle = .light
+        default: window?.overrideUserInterfaceStyle = .unspecified
+        }
+    }
+}
+
+fileprivate enum Theme: String, Sendable {
+    case system
+    case light
+    case dark
+}
+
+fileprivate extension UserDefaults {
+    var theme: Theme {
+        let themeString = string(forKey: "CapacitorStorage.appTheme")
+        guard let themeString else { return .system }
+        return switch(themeString.lowercased()) {
+        case "light": .light
+        case "dark": .dark
+        case "system": .light
+        default: .system
+        }
+    }
 }
--- example/ios/App/CapApp-SPM/Package.swift
+++ example/ios/App/CapApp-SPM/Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "7.0.1"),
         .package(name: "CapacitorCamera", path: "../../../node_modules/@capacitor/camera"),
+        .package(name: "CapacitorPreferences", path: "../../../node_modules/@capacitor/preferences"),
         .package(name: "CapacitorSplashScreen", path: "../../../node_modules/@capacitor/splash-screen"),
         .package(name: "UaCapacitorAirship", path: "../../../..")
     ],
@@ -23,6 +24,7 @@ let package = Package(
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "CapacitorCamera", package: "CapacitorCamera"),
+                .product(name: "CapacitorPreferences", package: "CapacitorPreferences"),
                 .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen"),
                 .product(name: "UaCapacitorAirship", package: "UaCapacitorAirship")
             ]
