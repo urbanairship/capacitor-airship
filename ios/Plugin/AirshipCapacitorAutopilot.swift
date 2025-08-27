9647b06d2ebe48d9dc7ab59b3e4c1e8c1c2e3c12
Ryan Lepinski <ryan.lepinski@airship.com>
2025-08-27 11:30:45 -0700

    Fix deprecations

--- ios/Plugin/AirshipCapacitorAutopilot.swift
+++ ios/Plugin/AirshipCapacitorAutopilot.swift
@@ -16,7 +16,6 @@ public final class AirshipCapacitorAutopilot: NSObject {
 
     private var pluginInitialized: Bool = false
     private var applicationDidFinishLaunching: Bool = false
-    private var launchOptions: [UIApplication.LaunchOptionsKey : Any]?
 
     fileprivate var pluginConfig: PluginConfig?
 
@@ -32,11 +31,8 @@ public final class AirshipCapacitorAutopilot: NSObject {
      */
 
     @MainActor
-    func onApplicationDidFinishLaunching(
-        launchOptions: [UIApplication.LaunchOptionsKey : Any]?
-    ) {
+    func onApplicationDidFinishLaunching() {
         AirshipProxy.shared.delegate = self 
-        self.launchOptions = launchOptions
         self.applicationDidFinishLaunching = true
         if self.pluginInitialized, self.applicationDidFinishLaunching {
             try? AirshipProxy.shared.attemptTakeOff(
@@ -60,7 +56,6 @@ public final class AirshipCapacitorAutopilot: NSObject {
     func attemptTakeOff(json: Any) throws -> Bool {
         return try AirshipProxy.shared.takeOff(
             json: json,
-            launchOptions: self.launchOptions
         )
     }
 }
